e3db5b73cdf043f8fecec6570faf3bcb89164df5
koushik0004 <koushik.sadhukhan0004@gmail.com>
2019-01-24 00:40:38 +0530

    updated new code with recurssion matrix spiral

---
+++ interview-assignment/matrix-spiral-reverse.24.js
@@ -0,0 +1,30 @@
+const givenArr1 = [
+  [1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 10, 11, 12],
+  [13, 14, 15, 16, 17, 18]
+];
+
+const givenArr2 = [
+  [1,  2,   3,  4],
+  [5,  6,   7,  8],
+  [9,  10, 11, 12],
+  [13, 14, 15, 16]
+];
+
+const reverseSpiral = (givenArr, resultArr) => {
+  if(givenArr.length === 0){
+    return resultArr;
+  }
+  resultArr = [
+    ...resultArr,
+    ...givenArr.shift(),
+    ...givenArr.map(item => item.pop()),
+    ...givenArr.pop().reverse(),
+    ...givenArr.map(item => item.shift()).reverse()
+  ];
+
+  reverseSpiral(givenArr, resultArr);
+};
+
+console.log(`Reverse order spiral for givenArr2: `, reverseSpiral(givenArr2.map(item => item.slice(0))), []);
+console.log([...givenArr2]);
