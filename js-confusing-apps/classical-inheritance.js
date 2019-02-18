5f950b844bfd8ad04459178369668481f3652ed6
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2019-02-18 17:25:28 +0530

    updated more info

--- js-confusing-apps/classical-inheritance.js
+++ js-confusing-apps/classical-inheritance.js
@@ -28,3 +28,35 @@ Circle1.prototype.area = function() {
 };
 const cr = new Circle1(5,6,7);
 console.log(`Circle area is - ${cr.area()}`);
+/**
+ * 
+ * refer to another link
+ * https://stackoverflow.com/questions/9122202/prototypal-inheritance-in-js-and-how-to-get-parent-properties
+ */
+
+////
+////
+const ctObj = {
+  name: {
+    firstName: 'koushik',
+    lastName: 'Sadhukhan'
+  },
+  address: {
+    residential: {
+      place: 'one',
+      streetname: 'mm',
+      pinode: '707'
+    },
+    permanent: {
+      place: 'another one',
+      streetname: 'mm',
+      pinode: '707'
+    }
+  }
+};
+let newObj={};
+for(single in ctObj){
+  cntr++;
+  newObj = {...newObj, [single]: ctObj[single]};
+}
+console.log(newObj);
